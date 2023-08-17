a45fb1434f7c9df7f3b6f7964f71bdf17eae53ea
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-08-17 16:22:07 -0300

    1 - Tarefa03

---
+++ 1 - Introdução à Programação/Tarefa03/script.js
@@ -0,0 +1,57 @@
+function CalcularPorcentagem(){
+    const valor = document.getElementById("tipo_vinho").value;
+    const p = document.getElementById("porcentagem");
+    var tipo_vinho;
+    var porcentagem;
+
+    //console.log(valor);
+
+    if (valor == "T"){
+        tipo_vinho = "Tinto";
+        porcentagem = 40;
+    }
+    if (valor == "B"){
+        tipo_vinho = "Branco";
+        porcentagem = 35;
+    }
+    if (valor == "R"){
+        tipo_vinho = "Rose";
+        porcentagem = 25;
+    }
+
+    p.innerHTML = `A porcentagem de vinho ${tipo_vinho} é ${porcentagem}%`;
+    
+}
+
+
+function CalcularPorcentagemSwitch(){
+    const valor = document.getElementById("tipo_vinho").value;
+    const p = document.getElementById("porcentagem");
+    var tipo_vinho;
+    var porcentagem;
+
+    console.log(valor);
+
+    switch(valor){
+        case "T":
+            tipo_vinho = "Tinto";
+            porcentagem = 40;
+            break;
+        
+        case "B":
+            tipo_vinho = "Branco";
+            porcentagem = 35;
+            break;
+
+        case "R":
+            tipo_vinho = "Rose";
+            porcentagem = 25;
+            break;
+
+        default:    
+            break;
+    }
+
+    p.innerHTML = `A porcentagem de vinho ${tipo_vinho} é ${porcentagem}%`;
+    
+}
