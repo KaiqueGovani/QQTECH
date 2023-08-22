ce81baac185574f677aec77d6334a2df2a9b0b20
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-08-22 18:44:07 -0300

    1 - Tarefa07 Loja v1

---
+++ 1 - Introdução à Programação/Tarefa07 Loja/script.js
@@ -0,0 +1,11 @@
+try {document.getElementById("btnCadastrar").addEventListener("click", () => {
+    window.location.href = "cadastrar.html";
+    console.log("Cadastrar");
+})} catch (error) {
+    console.log(error);
+}
+
+function Cadastrar()
+{
+    console.log("Cadastrar");
+}
