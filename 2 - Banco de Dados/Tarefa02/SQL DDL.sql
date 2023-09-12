19b7df35d588ccc9d1143738aee28f8fd7b86cc2
KaiqueGovani <kaique.govani@hotmail.com>
2023-09-12 16:48:41 -0300

    2 - Tarefa03

--- 2 - Banco de Dados/Tarefa02/SQL DDL.sql
+++ 2 - Banco de Dados/Tarefa02/SQL DDL.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS cliente CASCADE;
+DROP TABLE IF EXISTS categoria CASCADE;
+DROP TABLE IF EXISTS produto CASCADE;
+DROP TABLE IF EXISTS vendedor CASCADE;
+DROP TABLE IF EXISTS pedido CASCADE;
+DROP TABLE IF EXISTS pedido_produto CASCADE;
+
 CREATE TABLE cliente (
     id serial PRIMARY KEY,
     nome varchar(255) NOT NULL,
@@ -29,12 +36,14 @@ CREATE TABLE pedido (
     id serial PRIMARY KEY,
     cliente_id integer REFERENCES cliente (id) NOT NULL,
     data_de_criacao DATE NOT NULL,
-    vendedor_id integer REFERENCES vendedor (id) NOT NULL,
-    valor_pedido DECIMAL(10,2) NOT NULL
+    vendedor_id integer REFERENCES vendedor (id)
 );
+
+ALTER TABLE pedido ALTER COLUMN data_de_criacao TYPE Timestamp; 
 
 CREATE TABLE pedido_produto (
     pedido_id integer REFERENCES pedido (id) NOT NULL,
     produto_id integer REFERENCES produto (id) NOT NULL,
+    valor_produto DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (pedido_id, produto_id)
 );
