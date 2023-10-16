c3305a089fe05dc84d49eb2a70b309ecc52bed99
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-10-16 02:38:09 -0300

    Comentando ideias

--- Projeto Bê-á-Bá/Projeto-Docker/Vallidator/backend/middlewares/verificarPermissao.js
+++ Projeto Bê-á-Bá/Projeto-Docker/Vallidator/backend/middlewares/verificarPermissao.js
@@ -1,3 +1,4 @@
+//? Analizar o a verificação pelo banco de dados ao inves do token ?
 module.exports = (req, res, next) => {
     if (req.permissao !== 'admin') {
         res.status(403).json({ mensagem: 'Você não tem permissão para acessar este recurso' });
