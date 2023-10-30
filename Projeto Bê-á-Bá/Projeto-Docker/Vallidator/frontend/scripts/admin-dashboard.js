1c904b6cfcbc196c375db7d1a7a459b13e521f65
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-10-30 05:31:34 -0300

    [dashboard] Graficos Templates

--- Projeto Bê-á-Bá/Projeto-Docker/Vallidator/frontend/scripts/admin-dashboard.js
+++ Projeto Bê-á-Bá/Projeto-Docker/Vallidator/frontend/scripts/admin-dashboard.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    popularTabelas();
+document.addEventListener('DOMContentLoaded', async function () {
+    await popularTabelas();
+    await desenharGraficos();
 });
 
 async function popularTabelas() {
@@ -26,16 +27,16 @@ async function popularTemplates() {
                     <td>${template.extensao.toUpperCase()}</td>
                     <td>${template.id_criador}</td>
                     <td>${new Date(template.data_criacao).toLocaleDateString()}</td>
-                    <th>${template.status ? 
-                        `<div class="text-white rounded-5 text-center p-1"
+                    <th>${template.status ?
+                    `<div class="text-white rounded-5 text-center p-1"
                             style="background-color: var(--Terciaria);">Ativo
-                        </div>` 
-                        : (template.status === null ? `<div class="rounded-5 text-center p-1" style="background-color: #FFFAAA;">
-                        Revisão</div>`
-                        : 
+                        </div>`
+                    : (template.status === null ? `<div class="rounded-5 text-center p-1" style="background-color: #FFFAAA;">
+                        Pendente</div>`
+                        :
                         `<div class="rounded-5 text-center p-1" style="background-color: var(--Baby-Green);">
                         Inativo</div>`)
-                        }
+                }
                     </th>
                 </tr>
                 `;
@@ -73,18 +74,74 @@ async function popularUploads() {
     }
 }
 
+async function desenharGraficos() {
+    await desenharGraficoTemplates();
+    //await desenharGraficoUploads();
+}
+
+async function desenharGraficoTemplates() {
+    const response = await fetch('/templates/data')
+    const data = await response.json();
+
+    console.log(data);
+
+    const ctx = document.getElementById('graficoTemplates1').getContext('2d');
+    const grafico1 = new Chart(ctx, {
+        type: 'pie',  // ou 'doughnut' para um gráfico de rosca
+        data: {
+            labels: ['Ativos', 'Inativos', 'Pendentes'],
+            datasets: [{
+                label: 'Templates',
+                data: [data.ativo, data.inativo, data.pendente],
+                backgroundColor: ['#36965E', '#B4EFCC', '#FFFAAA']
+            },
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+            }
+        }
+    });
+
+    const ctx2 = document.getElementById('graficoTemplates2').getContext('2d');
+    const grafico2 = new Chart(ctx2, {
+        type: 'pie',  // ou 'doughnut' para um gráfico de rosca
+        data: {
+            labels: ['CSV', 'XLS', 'XLSX'],
+            datasets: [{
+                label: 'Formatos',
+                data: [data.csv, data.xls, data.xlsx],
+                backgroundColor: ['#FFAAAA', '#FADFAD', '#AABBFF']
+            },
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+            }
+        }
+    });
+
+}
+
 function removeTimestampFromFilename(filename) {
     // Extrai a extensão do arquivo
     let extensao = filename.split('.').pop();
 
     // Remove a extensão do arquivo
     let semExtensao = filename.split('.').slice(0, -1).join('.');
-    
+
     // Encontra a penúltima ocorrência de '_'
     let ultimoIndice = semExtensao.lastIndexOf('_');
     let penultimoIndice = semExtensao.lastIndexOf('_', ultimoIndice - 1);
-    
+
     // Retorna a string até a penúltima '_'
     return semExtensao.substring(0, penultimoIndice) + '.' + extensao;
 }
-
