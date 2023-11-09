d34ded05a92f2e4fa19c8cba9bfdbc8be5bf077f
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2023-11-09 02:35:39 -0300

    Human File Size

--- Projeto Bê-á-Bá/Projeto-Docker/Vallidator/frontend/scripts/admin-dashboard.js
+++ Projeto Bê-á-Bá/Projeto-Docker/Vallidator/frontend/scripts/admin-dashboard.js
@@ -36,7 +36,7 @@ async function popularTemplates() {
                         :
                         `<div class="rounded-5 text-center p-1" style="background-color: var(--Baby-Green);">
                         Inativo</div>`)
-                    }
+                }
                     </th>
                 </tr>
                 `;
@@ -65,7 +65,7 @@ async function popularUploads() {
                     <td>${arquivo.id_template}</td>
                     <td>${arquivo.id_usuario}</td>
                     <td>${new Date(arquivo.data).toLocaleDateString()}</td>
-                    <td>${arquivo.tamanho_bytes}</td>
+                    <td>${humanFileSize(arquivo.tamanho_bytes, true)}</td>
                 </tr>
                 `;
         }
@@ -173,4 +173,36 @@ function removeTimestampFromFilename(filename) {
 
     // Retorna a string até a penúltima '_'
     return semExtensao.substring(0, penultimoIndice) + '.' + extensao;
+}
+
+/**
+ * Format bytes as human-readable text.
+ * 
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * 
+ * @return Formatted string.
+ */
+function humanFileSize(bytes, si = false, dp = 1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+    return bytes.toFixed(dp) + ' ' + units[u];
 }
